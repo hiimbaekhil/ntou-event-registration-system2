03bedb613ce3367f4962996c2dfecd3e575de7f6
baekhil <baekhil.20@gmail.com>
2023-10-17 00:19:33 +0800

    Create Spring Boot project

---
+++ ntou-event-registration/src/main/java/ntou/cse/ntoueventregistration/NtouEventRegistrationApplication.java
@@ -0,0 +1,13 @@
+package ntou.cse.ntoueventregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NtouEventRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NtouEventRegistrationApplication.class, args);
+    }
+
+}
---
+++ ntou-event-registration/src/test/java/ntou/cse/ntoueventregistration/NtouEventRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ntou.cse.ntoueventregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NtouEventRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
